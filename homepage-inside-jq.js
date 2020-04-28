0d100df0e02f0dc73bd6441bf5a80f84894748a1
mishalift130 <61660575+mishalift130@users.noreply.github.com>
2020-04-29 01:25:52 +0600

    Add files via upload

---
+++ homepage-inside-jq.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+    $(".siup").click(function(){
+      $(".login").hide();
+    });
+    $(".siup").click(function(){
+      $(".signin").show();
+    });
+    $(".gin").click(function(){
+      $(".signin").hide();
+    });
+    $(".gin").click(function(){
+      $(".login").show();
+    });
+     
+    $("#title1").click(function(){
+  $(".fullfrom").slideToggle(1000);
+  });
+  
+  });
